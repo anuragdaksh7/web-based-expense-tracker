1a5dc7912532deb696306183f78c7469f4c0b53d
anuragdaksh <anuragdaksh77777@gmail.com>
2023-06-16 19:39:36 +0530

    added index.js login.html signup.html and added image properly loding in server

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.static('assets'));
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+
+
+
+app.get("/", (req, res) => {
+    fs.readFile('index.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data);
+    });
+});
+
+
+app.get("/login", (req, res) => {
+    fs.readFile('login.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data);
+    });
+});
+
+
+app.get("/signup", (req, res) => {
+    fs.readFile('signup.html', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data);
+    });
+});
