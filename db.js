cb743d5fe1bf46c3ea54a6639c6441a54daf2bfd
__anuragdaksh7__ <84393491+anuragdaksh7@users.noreply.github.com>
2023-08-04 18:06:55 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,1 @@
-const mongoose = require("mongoose");
-const URL = require('./key.js');
-console.log(URL)
-mongoose.set('strictQuery', false);
-mongoose.connect(URL, { 
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(() => {
-    console.log("connection succsessfull");
-}).catch((e) => {
-    console.log(e);
-});
+
